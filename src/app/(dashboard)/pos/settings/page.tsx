157d86d5b0cbb0d8f42936e5ed659433368b00aa
Jujun Junawan <jujunjunawan2@gmail.com>
2026-07-18 16:27:19 +0700

    settingsPage

--- src/app/(dashboard)/pos/settings/page.tsx
+++ src/app/(dashboard)/pos/settings/page.tsx
@@ -1,9 +1,5 @@
+import SettingsPage from "@/modules/pos/settings/pages/SettingsPage";
+
 export default function Page() {
-  return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">
-        Dalam Pengembangan
-      </h1>
-    </div>
-  );
+  return <SettingsPage />;
 }
