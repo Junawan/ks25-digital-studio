4f5fa29481e15127dcf02935789b83e4b32d9aec
Jujun Junawan <jujunjunawan2@gmail.com>
2026-07-19 14:01:05 +0700

    fix form2

--- src/modules/pos/settings/pages/SettingsPage.tsx
+++ src/modules/pos/settings/pages/SettingsPage.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import SettingsContent from "../components/SettingsContent";
-import { PosSettingsProvider } from "../context/PosSettingsContext";
 
 
 export default function SettingsPage() {
   return (
-    <PosSettingsProvider>
+    
       <SettingsContent />
-    </PosSettingsProvider>
+  
   );
 }
